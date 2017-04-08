4c14d040e13e142841ec1bced1870c8fd15e50aa
Rishikanth <rishikanthc@gmail.com>
2017-04-08 06:59:05 -0400

    Added a leaderboard | split into energyDashboard and sampletv

--- jobs/fitbit_leaderboard.rb
+++ jobs/fitbit_leaderboard.rb
@@ -1,7 +1,7 @@
 # fitbit = Fitbit.new
 
 SCHEDULER.every "2s" do
-	send_event "fitbit_leaderboard", { people: [{rank: 1, steps: "--", name: "ICSL", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/bicsl.png", style: ""}, {rank: 1, steps: "--", name: "Danino Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}, {rank: 1, steps: "--", name: "Burke's Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}, {rank: 1, steps: "--", name: "Teherani's Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}]}
+	send_event "fitbit_leaderboard", { people: [{rank: 1, steps: "--", name: "ICSL", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/bicsl.png", style: ""}, {rank: 2, steps: "--", name: "Danino Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}, {rank: 3, steps: "--", name: "Burke's Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}, {rank: 4, steps: "--", name: "Teherani's Lab", avatar: "http://icsl.ee.columbia.edu:3030/assets/fitbit/logo-placeholder.jpg", style: ""}]}
   # if fitbit.errors?
   #   send_event "fitbit_leaderboard", { error: fitbit.error }
   # else
