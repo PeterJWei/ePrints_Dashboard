815330944fbef6ca5a16400e3685dc2cac44d691
Rishikanth <rishikanthc@gmail.com>
2017-04-14 07:36:33 -0400

    Trial of energy usage over week

--- jobs/weekChart.rb
+++ jobs/weekChart.rb
@@ -16,26 +16,26 @@ url = URI.parse("http://icsl.ee.columbia.edu:8000/api/appSupport/buildingFootpri
 data = [['Time', 'Energy'], [0, 0]]
 
 
-(1..500).each do |i|
+(1..1000).each do |i|
 	h, m, s = Time.now.strftime("%H:%M:%S").split(":").map(&:to_i)
 	#h = (h - 4) % 24
-	data << [[h, m, s], 0, 0]
+	data << [i, rand(10000..60000)]
 end
 #x = data.last[0]
-x = 500
-SCHEDULER.every '2s' do
+x = 1000
+SCHEDULER.every '10s' do
 	response = Net::HTTP.get_response(url)
 	parsed = JSON.parse(response.body)
-	x += 2
+	x += 1
 	timeval = Time.at(x).utc.strftime("%H:%M:%S")
 	h, m, s = Time.now.strftime("%H:%M:%S").split(":").map(&:to_i)
 	#h = (h - 4) % 24
-	dataPoint = [[h, m, s], parsed["HVAC"], parsed["Light"], parsed["Electrical"]]
+	dataPoint = [x, parsed["value"]]
 	data.shift
-	data[0] = ['Time', 'HVAC', 'Lights', 'Plugs']
+	data[0] = ['Time', 'Power']
 	data << dataPoint
 	#puts data[0]
-	send_event('mychart', points: data)
+	send_event('weekchart', points: data)
 	#send_event('testchart', points: data)
 	#send_event('testchart', points2: data)
 end
